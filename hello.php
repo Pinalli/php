089b7fba5416a59351164954027967bdd8df0be2
Pinalli <betopinalli@gmail.com>
2021-01-09 10:49:40 -0300

    Initial commit

---
+++ hello.php
@@ -0,0 +1,23 @@
+<!DOCTTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exemplo PHP</title>
+        <style>
+            h2 {
+                color: #80a2ff;
+                text-shadow: 1px 1px 1px black;
+            }
+            h1 {
+                color: #110d58;
+                text-shadow: 1px 1px 1px whitesmoke;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Testando PHP</h1>
+    <?php
+        echo "<h2>Hello, <br> World!</h2>";
+    ?>
+    </body>
+</html>
